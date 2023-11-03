725d25003e2ce47586da1ed8c730ba21079caf41
Anthony Le <an.tho.le95@gmail.com>
2023-11-03 18:14:13 +0000

    add psycopg2 requirement

--- app.py
+++ app.py
@@ -1,6 +1,15 @@
 from flask import Flask
+import psycopg2
+
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
     return 'Hello, World!'
+
+@app.route('/db_test')
+def testing():
+    conn = psycopg2.connect("postgres://lab_10_database_yrpp_user:TG46YdBQCWePmjwREmkUdOsu0eQP9LEM@dpg-cl2j5kg310os73b81c9g-a.oregon-postgres.render.com/lab_10_database_yrpp")
+    conn.close()
+    return "Database Connection Successful"
+
